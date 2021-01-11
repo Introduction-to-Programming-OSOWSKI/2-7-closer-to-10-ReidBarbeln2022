43de450db35744adb3397dba4bd836aedbcb2121
ReidBarbeln2022 <814684@mystma.org>
2021-01-11 15:57:44 +0000

    close 10

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    if abs (x - 10) < abs(y - 10):
+        return x
+    elif x == y:
+        return 0
+   
+    else:
+        return y
+    
+    
+print (close10(9, 9))
+
