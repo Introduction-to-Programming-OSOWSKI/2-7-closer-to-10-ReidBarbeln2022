623d8a0c2d3415a3dee4719cde62fd455b16249f
ReidBarbeln2022 <814684@mystma.org>
2021-01-11 16:02:35 +0000

    close 10 try2

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def close10(x, y):
     if abs (x - 10) < abs(y - 10):
         return x
-    elif x == y:
+    elif  abs (x - 10) == abs(y - 10):
         return 0
    
     else:
         return y
     
     
-print (close10(9, 9))
+print (close10(5, 15))
 
